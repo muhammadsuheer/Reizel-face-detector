07bff50383a572d3b6d72113bb9d1dd811584ae2
muhammadsuheer <58456635+muhammadsuheer@users.noreply.github.com>
2020-03-05 23:12:08 +0500

    Updates

---
+++ precache-manifest.a4c254e4fae34919e46a9c5b848607f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e8392e0463d28a93c6ae1351d16c7e",
+    "url": "/Reizel-face-detector/index.html"
+  },
+  {
+    "revision": "310af3331e97b6529bb0",
+    "url": "/Reizel-face-detector/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dd3a6fe87f10a6738ee3",
+    "url": "/Reizel-face-detector/static/css/main.edb211e7.chunk.css"
+  },
+  {
+    "revision": "310af3331e97b6529bb0",
+    "url": "/Reizel-face-detector/static/js/2.277b8094.chunk.js"
+  },
+  {
+    "revision": "5494a601d814958c16822201344f27e7",
+    "url": "/Reizel-face-detector/static/js/2.277b8094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3a6fe87f10a6738ee3",
+    "url": "/Reizel-face-detector/static/js/main.340859bf.chunk.js"
+  },
+  {
+    "revision": "8fd595532bb2ca98b917",
+    "url": "/Reizel-face-detector/static/js/runtime-main.a48904ab.js"
+  }
+]);
